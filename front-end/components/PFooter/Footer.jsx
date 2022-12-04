9ba86e9283196b33c0c5704d263c2e6b11b2d2a4
Ozy <munkhtamir.g@outlook.com>
2022-12-04 21:02:17 +0800

    MOD

--- front-end/components/PFooter/Footer.jsx
+++ front-end/components/PFooter/Footer.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full flex justify-center items-center">
+      <p>
+        {" "}
+        Developed by <span className="font-semibold"> Ozy</span>
+      </p>
+    </div>
+  );
 }
