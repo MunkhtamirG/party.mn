73d2254494d6a1d2f2982cd85f83768e686ddb7b
Ozy <munkhtamir.g@outlook.com>
2022-12-08 21:38:43 +0800

    MOD

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,35 +1,52 @@
-import React from "react";
-import { Select } from "antd";
+import React, { useState } from "react";
+import { Select, Steps } from "antd";
 
 export default function Index() {
-  const handleChange = (value) => {
+  const [stepper, setStepper] = useState(0);
+  const chooseType = (value) => {
     console.log(`selected ${value}`);
   };
+  const stepHandler = (value) => {
+    setStepper(value);
+  };
+  const items = [
+    { title: "Төрөлөө сонгоно уу!" },
+    { title: "second step" },
+    { title: "third step" },
+  ];
+
   return (
-    <div className="main text-white flex flex-col gap-5 justify-center items-center h-[100%]">
-      <p className="text-3xl font-semibold">Төрөлөө сонгоно уу!</p>
-      <Select
-        defaultValue="choose"
-        onChange={handleChange}
-        options={[
-          {
-            value: "choose",
-            label: "Төрөлөө сонгоно уу!",
-          },
-          {
-            value: "birthday",
-            label: "Төрсөн өдөр",
-          },
-          {
-            value: "disabled",
-            label: "Disabled",
-          },
-          {
-            value: "Yiminghe",
-            label: "yiminghe",
-          },
-        ]}
-      />
-    </div>
+    <>
+      <div className="main text-white flex flex-col gap-5 justify-around items-center h-[100%]">
+        <div>
+          <p className="text-3xl font-semibold">Төрөлөө сонгоно уу!</p>
+          <Select
+            defaultValue="choose"
+            onChange={chooseType}
+            className="w-[100%] mt-5"
+            options={[
+              {
+                value: "choose",
+                label: "Төрөлөө сонгоно уу!",
+              },
+              {
+                value: "birthday",
+                label: "Төрсөн өдөр",
+              },
+              {
+                value: "anniversary",
+                label: "Ойн баяр",
+              },
+            ]}
+          />
+        </div>
+        <Steps
+          items={items}
+          className="px-20 text-white"
+          onChange={stepHandler}
+          current={stepper}
+        />
+      </div>
+    </>
   );
 }
